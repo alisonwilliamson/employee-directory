155c90a1403fa885552c35f7f9750c1c2dbac974
Allie <williamson.alisonj@gmail.com>
2020-10-26 23:18:10 -0600

    installed material icons/added link to index.html to fix table layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import Table from './components/Table';
+import Container from '@material-ui/core/Container';
 
 function App() {
   return (
-      <>
-        <NavBar />
+    <>
+      <NavBar />
+      <Container fixed>
         <Table />
-      </>
+      </Container>
+    </>
   );
 }
 
